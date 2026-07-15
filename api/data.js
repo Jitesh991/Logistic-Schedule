acb637c39fc64de806f3abd919c80c5cba30add6
Jitesh991 <operations@sunbeamsimpexinc.com>
2026-07-15 15:41:38 +0800

    index.html

--- api/data.js
+++ api/data.js
@@ -43,6 +43,7 @@ async function writeCol(col, data) {
   if (blobs.length) await Promise.all(blobs.map(b => del(b.url)));
   await put(`sii/${col}.json`, JSON.stringify(data), {
     addRandomSuffix: false,
+    access: 'private',
     contentType: 'application/json'
   });
 }
